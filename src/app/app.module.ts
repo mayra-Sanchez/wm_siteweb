24e1f9701ee3cc55e29aba878009a3b6914dc105
mayra-Sanchez <mayra.alejandra.sanchez@correounivalle.edu.co>
2024-12-06 08:41:37 -0500

    Add header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,44 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminDashboardComponent } from './components/Admin/admin-dashboard/admin-dashboard.component';
+import { ProductListComponent } from './components/Admin/product-list/product-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ProductFormComponent } from './components/Admin/product-form/product-form.component';
+import { SalesReportComponent } from './components/Admin/sales-report/sales-report.component';
+import { ProductCatalogComponent } from './components/Custom/product-catalog/product-catalog.component';
+import { ProductDetailComponent } from './components/Custom/product-detail/product-detail.component';
+import { CartComponent } from './components/Custom/cart/cart.component';
+import { FavoritesComponent } from './components/Custom/favorites/favorites.component';
+import { PurchaseHistoryComponent } from './components/Custom/purchase-history/purchase-history.component';
+import { LoginComponent } from './components/login/login.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MisionComponent } from './components/mision/mision.component';
+import { VisionComponent } from './components/vision/vision.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminDashboardComponent,
+    ProductListComponent,
+    ProductFormComponent,
+    SalesReportComponent,
+    ProductCatalogComponent,
+    ProductDetailComponent,
+    CartComponent,
+    FavoritesComponent,
+    PurchaseHistoryComponent,
+    LoginComponent,
+    HeaderComponent,
+    FooterComponent,
+    MisionComponent,
+    VisionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
